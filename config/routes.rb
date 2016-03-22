f303c0a08b15775384953c37549306de683d43bf
Igor C. Batista <igor.batista@gmail.com>
2016-03-22 11:35:01 -0300

    git ignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cost_centers
   resources :drivers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
