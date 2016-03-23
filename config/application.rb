6c67873c32cd51cbd1ae31fd59bcd97cb2dfb0af
Igor C. Batista <igor.batista@gmail.com>
2016-03-23 13:01:52 -0300

    config assets

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Servosfinan
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_controller.include_all_helpers = false
+    config.time_zone = 'Brasilia'
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
