dee74b62c1f2d4058a421ab1fb216b0e8d5d4c18
Igor C. Batista <igor.batista@gmail.com>
2016-03-23 11:41:32 -0300

    fisih layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def after_sign_in_path_for(resource)
-    account_payables_path
+    account_receivables_path
   end
 end
